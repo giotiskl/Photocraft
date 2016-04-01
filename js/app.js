8063f2b7684f2fa35c0d7c0b8ee0b71b291a86ec
Giotis Kal <giotisgr@gmail.com>
2016-04-01 19:53:39 +0300

    Added imagesLoaded script to use with Filterizr

--- js/app.js
+++ js/app.js
@@ -28,37 +28,39 @@ $(function() {
     /*******************************
     *            PORTFOLIO
     ********************************/
-    //Initialize controls
-    $('.portfolio .portfolio-btn').click(function() {
-        $(this).toggleClass('active');
-    });
-    //Initialize Filterizr
-    var fltr = $('.filtr-container').filterizr({
-        delay: 25,
-        filterOutCss: {
-            opacity: 0,
-            transform: 'scale(0.75)',
-        },
-        filterInCss: {
-            opacity: 1,
-            transform: 'scale(1)',
-        },
-        layout: 'sameWidth',
-    });
-    //Initialize Swipebox
-    $('.filtr-item').click(function() {
-        //Update swipebox links based on whether the item is filtered out or not
-        $('.filtr-item').each(function(i, e) {
-            var item = $(e),
-                isFilteredOut = item.hasClass('filteredOut');
-                itemLink = $(e).find('a');
-
-            if (isFilteredOut)
-                itemLink.removeClass('swipebox');
-            else
-                itemLink.addClass('swipebox');
+    $('.portfolio').imagesLoaded(function() {
+        //Initialize controls
+        $('.portfolio .portfolio-btn').click(function() {
+            $(this).toggleClass('active');
         });
-        $('.swipebox').swipebox();
+        //Initialize Filterizr
+        var fltr = $('.filtr-container').filterizr({
+            delay: 25,
+            filterOutCss: {
+                opacity: 0,
+                transform: 'scale(0.75)',
+            },
+            filterInCss: {
+                opacity: 1,
+                transform: 'scale(1)',
+            },
+            layout: 'sameWidth',
+        });
+        //Initialize Swipebox
+        $('.filtr-item').click(function() {
+            //Update swipebox links based on whether the item is filtered out or not
+            $('.filtr-item').each(function(i, e) {
+                var item = $(e),
+                    isFilteredOut = item.hasClass('filteredOut');
+                    itemLink = $(e).find('a');
+
+                if (isFilteredOut)
+                    itemLink.removeClass('swipebox');
+                else
+                    itemLink.addClass('swipebox');
+            });
+            $('.swipebox').swipebox();
+        });
     });
 
     /*******************************
